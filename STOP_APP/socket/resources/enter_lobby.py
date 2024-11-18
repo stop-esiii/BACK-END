103dd1fd02e73ae64d60f6079c838b687e80852f
little-junior <eulittlejunior@gmail.com>
2024-11-18 13:18:25 -0300

    add prints entry_lobby

--- STOP_APP/socket/resources/enter_lobby.py
+++ STOP_APP/socket/resources/enter_lobby.py
@@ -3,7 +3,7 @@ from STOP_APP.sql.services import LobbyService
 from STOP_APP.sql.models import User
 from STOP_APP.socket.models import lobby_users
 from sqlalchemy import and_
-
+import logging
 
 def handle_enter_lobby(socketio, data):
     # Persist lobby
@@ -20,7 +20,7 @@ def handle_enter_lobby(socketio, data):
 
     # Appending client to the room
     join_room(result["lobby"].code_lobby)
-
+    logging.warning("JOGADOR ENTROU NO LOBBY")
     # Get username by "id_user"
     username = User().query.filter(and_(User.id==data["id_user"], User.active==True)).first().username
     # Get users in lobby_users
