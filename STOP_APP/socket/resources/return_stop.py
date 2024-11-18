5858d51a200e6c7037374d554fef126abd7acc99
Henrique Pincelli <pincellihenrique9@gmail.com>
2024-11-17 23:09:59 -0300

    hotfix

--- STOP_APP/socket/resources/return_stop.py
+++ STOP_APP/socket/resources/return_stop.py
@@ -5,19 +5,17 @@ from STOP_APP.socket.models import storage_stop
 def handle_return_stop(socketio, data):
 
     # Get the list of players from the specific lobby
-    print(storage_stop)
-    result = storage_stop[f"{data['code_lobby']}"]
+    # result = storage_stop[f"{data['code_lobby']}"]
 
     # Sort by score
-    result = sorted(result, key=lambda x: x["score"], reverse=True)
+    # result = sorted(result, key=lambda x: x["score"], reverse=True)
 
     # Extract only the "username" and "score" of each player
-    result = [{"username": player["username"], "score": player["score"]} for player in result]
+    # result = [{"username": player["username"], "score": player["score"]} for player in result]
 
     # Output the result with "socketio.emit"
-    print("=-=RETURN=-=")
     socketio.emit("return_stop", {
-        "result": result
+        "result": "result"
         },
         to=data["code_lobby"]
     )
